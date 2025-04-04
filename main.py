219cee9704c767353bb2e3b2618a35b9813ff628
luisBatis <roberdoogarcia@gmail.com>
2025-04-04 15:59:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-from minMax1 import minmax as m1
-from minMax2 import minmax as m2
-from minMax3 import minmax as m3
+resultado = 1
+qDePaisagens = input("teste")
 
-import timeit
-import random
+paisagens: list[str | int] = input().split(' ')
 
-tempo = {"m1": [], "m2": [], "m3": []}
 
-def criar_vetor(k):
-   lista = list()
-   for i in range(k):
-      lista.append(random.randint(-16000, 16000))
-   return lista
-
-for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
-   vetor = criar_vetor(i)
-   t_m1 = timeit.timeit(lambda: m1(vetor), number=1)
-   t_m2 = timeit.timeit(lambda: m2(vetor), number=1)
-   t_m3 = timeit.timeit(lambda: m3(vetor), number=1)
-   tempo["m1"].append(t_m1)
-   tempo["m2"].append(t_m2)
-   tempo["m3"].append(t_m3)
-
-print(tempo)
+paisagens = list(map(int, paisagens))
+for i in range(len(paisagens)):
+    if i ==0:
+        i = 2
+    if  paisagens[i] > paisagens[i-1] <  paisagens[i-2] or   paisagens[i] < paisagens[i - 1] > paisagens[i-2]:
+       resultado = 1 
+    else:
+         resultado = 0 
+   
+    i +=1
+print(resultado)
